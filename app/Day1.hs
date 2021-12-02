e464e7fb16bfd299fc65d76970de8fb6d498816e
Robbert van der Helm <mail@robbertvanderhelm.nl>
2021-12-02 15:38:22 +0100

    Add the solution for day 1 part 1

--- app/Day1.hs
+++ app/Day1.hs
@@ -1,4 +1,17 @@
 module Main where
 
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+  input <- parse <$> readFile "inputs/day-1.txt"
+
+  putStrLn "Part 1:"
+  print $! countIncrements input
+
+countIncrements :: [Int] -> Int
+countIncrements (x : y : xs)
+  | y > x     = countIncrements (y : xs) + 1
+  | otherwise = countIncrements (y : xs)
+countIncrements _ = 0
+
+parse :: String -> [Int]
+parse = map read . lines
