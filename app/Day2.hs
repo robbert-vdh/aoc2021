82f2b9b08514e92fa8da0859aaa3020f1421326f
Robbert van der Helm <mail@robbertvanderhelm.nl>
2021-12-02 16:24:31 +0100

    Add the solution for day 2 part 2

--- app/Day2.hs
+++ app/Day2.hs
@@ -11,6 +11,9 @@ main = do
   putStrLn "Part 1:"
   print . mulState $! eval1 input initialState
 
+  putStrLn "\nPart 2:"
+  print . mulState $! eval2 input initialState
+
 data State = State
   { position :: {-# UNPACK #-} !Int
   , depth    :: {-# UNPACK #-} !Int
@@ -31,12 +34,21 @@ data Command
   | Up Int      -- ^ _Decrease_ depth by n units
   | Down Int    -- ^ _Increase_ depth by n units
 
+-- | The evaluator for part 1, since the rules change in part 2. Oh and
 -- MonadState has not been invented yet in whatever the current year is!
-eval :: [Command] -> State -> State
-eval (Forward n : cs) s@State{..} = eval cs $! s { position = position + n }
-eval (Up n      : cs) s@State{..} = eval cs $! s { depth    = depth - n }
-eval (Down n    : cs) s@State{..} = eval cs $! s { depth    = depth + n }
-eval []               s           = s
+eval1 :: [Command] -> State -> State
+eval1 (Forward n : cs) s@State{..} = eval1 cs $! s { position = position + n }
+eval1 (Up n      : cs) s@State{..} = eval1 cs $! s { depth    = depth - n }
+eval1 (Down n    : cs) s@State{..} = eval1 cs $! s { depth    = depth + n }
+eval1 []               s           = s
+
+-- | In part 2 Up/Down control the ship's bearing, and the depth is determined
+-- by a combination of that and forwards movement.
+eval2 :: [Command] -> State -> State
+eval2 (Forward n : cs) s@State{..} = eval2 cs $! s { position = position + n, depth = depth + (aim * n) }
+eval2 (Up n      : cs) s@State{..} = eval2 cs $! s { aim      = aim - n }
+eval2 (Down n    : cs) s@State{..} = eval2 cs $! s { aim      = aim + n }
+eval2 []               s           = s
 
 parse :: String -> [Command]
 parse = map (go . words) . lines
@@ -44,4 +56,4 @@ parse = map (go . words) . lines
     go ["forward", read -> n] = Forward n
     go ["up"     , read -> n] = Up n
     go ["down"   , read -> n] = Down n
-    go xs                      = error "What are you even doing"
+    go xs                     = error "What are you even doing"
