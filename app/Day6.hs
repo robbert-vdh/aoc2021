c0054dea94bc0c7f3272af18172b64744adf7a8a
Robbert van der Helm <mail@robbertvanderhelm.nl>
2021-12-06 19:17:17 +0100

    Add a vector based implementation for day 6
    Because why not.

--- app/Day6.hs
+++ app/Day6.hs
@@ -2,10 +2,18 @@
 
 module Main where
 
-import Data.List (foldl')
-import Data.List.Split (splitOn)
 import Data.IntMap.Strict (IntMap)
 import qualified Data.IntMap.Strict as IM
+import Data.List (foldl')
+import Data.List.Split (splitOn)
+
+-- And a vector based implementation, because why not
+import Control.Monad (replicateM_)
+import Control.Monad.ST (ST)
+import Criterion.Main
+import Data.Vector.Unboxed (Vector)
+import qualified Data.Vector.Unboxed as V
+import qualified Data.Vector.Unboxed.Mutable as MV
 
 main :: IO ()
 main = do
@@ -19,12 +27,28 @@ main = do
   putStrLn "\nPart 2:"
   print . sum . doTimes evalFishMap 256 $! toMap input
 
+  -- Because why not
+  putStrLn "\nPart 1 GOTTA GO FAST EDITION:"
+  print . V.sum . evalVecSPEED 80  $! toVec input
+  putStrLn "\nPart 2 GOTTA GO FAST EDITION:"
+  print . V.sum . evalVecSPEED 256 $! toVec input
+
+  putStrLn "\nBenchmarks below:\n"
+  defaultMain
+    [ bench "linked list, 80 days" $ nf (length . doTimes evalFish 80)  input
+    , bench "intmap, 256 days"     $ nf (sum . doTimes evalFishMap 256) $! toMap input
+    , bench "I AM SPEED, 80 days"  $ nf (V.sum . evalVecSPEED 80)  $! toVec input
+    , bench "I AM SPEED, 256 days" $ nf (V.sum . evalVecSPEED 256) $! toVec input
+    ]
+
 doTimes :: (a -> a) -> Int -> a -> a
 doTimes f n = (!! n) . iterate f
 
 -- | The interval for lanternfish creating new lanternfish.
 spawnRate :: Int
 spawnRate = 7
+
+-- * Using lists
 
 -- | Run a single cycle of the fish spawning/updating algorithm. The numbers
 -- represent the amount of time remaining until a fish spawns another fish. When
@@ -35,6 +59,8 @@ evalFish :: [Int] -> [Int]
 evalFish (0 : ns) = 8 : 6 : evalFish ns
 evalFish (n : ns) = n - 1 : evalFish ns
 evalFish []       = []
+
+-- * Using intmaps
 
 toMap :: [Int] -> IntMap Int
 toMap = foldl' (\im n -> IM.insertWith (+) n 1 im) IM.empty
@@ -55,6 +81,30 @@ evalFishMap = doSpawns . decrementTimer
       $ im
       | otherwise
       = im
+
+-- * Using vectors
+
+toVec :: [Int] -> Vector Int
+toVec nums = V.modify (\mv -> mapM_ (MV.modify mv (+1)) nums) (V.replicate 10 0)
+
+-- | This does everything in place as a single monadic action. The day 9 here is
+-- used for the rollover from day 0 since we modify the vector in place and we
+-- iterate over the days in sequence, see below.
+evalVecSPEED :: Int -> Vector Int -> Vector Int
+evalVecSPEED times = V.modify $ \mv -> replicateM_ times $ mapM_ (modDayInPlace mv) [0..9]
+  where
+    modDayInPlace :: MV.MVector s Int -> Int -> ST s ()
+    modDayInPlace mv n = do
+      oldCount <- MV.unsafeRead mv n
+      MV.unsafeWrite mv n 0
+      if n == 0
+        then do
+          -- These are one higher than they would need to be, because we'll iterate
+          -- over them again, see above
+          MV.unsafeModify mv (+ oldCount) 7
+          MV.unsafeModify mv (+ oldCount) 9
+        else
+          MV.unsafeModify mv (+ oldCount) (n - 1)
 
 parse :: String -> [Int]
 parse = map read . splitOn ","
