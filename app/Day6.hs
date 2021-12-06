77bafcc85941dc59a53ccb134a42301c17251229
Robbert van der Helm <mail@robbertvanderhelm.nl>
2021-12-06 14:29:48 +0100

    Add the solution for day 6 part 2
    They pulled a sneaky on me.

--- app/Day6.hs
+++ app/Day6.hs
@@ -2,7 +2,10 @@
 
 module Main where
 
+import Data.List (foldl')
 import Data.List.Split (splitOn)
+import Data.IntMap.Strict (IntMap)
+import qualified Data.IntMap.Strict as IM
 
 main :: IO ()
 main = do
@@ -10,6 +13,11 @@ main = do
 
   putStrLn "Part 1:"
   print . length $ doTimes evalFish 80 input
+
+  -- I was going to just wait for the linked list approach to do its thing but
+  -- it took so long that I've already reimplemented it in the meantime
+  putStrLn "\nPart 2:"
+  print . sum . doTimes evalFishMap 256 $! toMap input
 
 doTimes :: (a -> a) -> Int -> a -> a
 doTimes f n = (!! n) . iterate f
@@ -27,6 +35,26 @@ evalFish :: [Int] -> [Int]
 evalFish (0 : ns) = 8 : 6 : evalFish ns
 evalFish (n : ns) = n - 1 : evalFish ns
 evalFish []       = []
+
+toMap :: [Int] -> IntMap Int
+toMap = foldl' (\im n -> IM.insertWith (+) n 1 im) IM.empty
+
+-- | The same as 'evalFish', but not using a linked list because they didn't
+-- want us to use fancy pattern matching.
+evalFishMap :: IntMap Int -> IntMap Int
+evalFishMap = doSpawns . decrementTimer
+  where
+    decrementTimer = IM.mapKeys (subtract 1)
+    -- Switching the order of spawning new fish and subtracting the timers
+    -- compared to 'evalFish' makes this much simpler
+    doSpawns im
+      | Just nRespawn <- IM.lookup (-1) im
+      = IM.insertWith (+) 8 nRespawn
+      . IM.insertWith (+) 6 nRespawn
+      . IM.delete (-1)
+      $ im
+      | otherwise
+      = im
 
 parse :: String -> [Int]
 parse = map read . splitOn ","
