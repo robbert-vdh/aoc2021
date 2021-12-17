7c133825e40abb130c3e8c88031c251e60522017
Robbert van der Helm <mail@robbertvanderhelm.nl>
2021-12-17 14:03:05 +0100

    Actually solve day 17 part 1
    There was an issue with my parser, that's why it seemed like there was
    no correct solution.

--- app/Day17.hs
+++ app/Day17.hs
@@ -1,52 +1,65 @@
 {-# LANGUAGE BangPatterns    #-}
 {-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE TupleSections   #-}
 
 module Main where
 
+import Data.List
+import Data.Maybe
+import Data.Ord
 import Numeric.GSL.SimulatedAnnealing
+import qualified Numeric.LinearAlgebra as H
 import Text.Parsec hiding (parse)
 import qualified Text.Parsec as Parsec
-import qualified Numeric.LinearAlgebra as H
 
 main :: IO ()
 main = do
   !input <- parse <$> readFile "inputs/day-17.txt"
 
-  putStrLn "So uh, this won't work because for my input the answer is unbounded, great"
-
   putStrLn "\nPart 1:"
   putStrLn $ "Target: " <> show input
-  print $! solve input
+  print . hitsTarget input $! fancySolve input
+  -- print $! bruteforce input
+
 
 -- | The target area.
-data Target = Target { left :: Int, right :: Int, top :: Int, bottom :: Int }
+data Target = Target { left :: Int, right :: Int, bottom :: Int, top :: Int }
   deriving (Show)
 
 -- | Find the velocity with the highest y-coordinate that passes through the
 -- target.
-solve :: Target -> (Int, Int)
-solve target =
-  simanSolve 420 2 solverParams (0, 0)
+--
+-- This doesn't actually work
+fancySolve :: Target -> (Int, Int)
+fancySolve target =
+  simanSolve 420 2 solverParams (20, 20)
     (solverPenalty target)
     (\(oldX, oldY) (newX, newY) -> fromIntegral $ abs (newX - oldX) + abs (newY - oldY))
     step
-    -- (Just $ show . overshootDistance target)
     (Just $ \velo -> show velo <> ": dist = " <> show (overshootDistance target velo))
   where
     solverParams = SimulatedAnnealingParams
-      50     -- Tries per step
-      30000  -- Tries per temperature
-      5      -- Maximum step size in random walk
+      1000   -- Tries per step
+      1000   -- Tries per temperature
+      1      -- Maximum step size in random walk
       1.0    -- Boltzman constant for random walks
-      20.0   -- Initial temperature
-      4.20   -- Cooling rate
+      2.0    -- Initial temperature
+      1.69   -- Cooling rate
       0.0069 -- Final temperature
 
     step :: H.Vector Double -> Double -> (Int, Int) -> (Int, Int)
-    step rands stepSize (veloX, veloY) =
-      ( veloX + round ((rands H.! 0) * stepSize)
-      , veloY + round ((rands H.! 1) * stepSize)
+    step rands _ velo@(veloX, veloY) =
+      ( veloX + round (((rands H.! 0) - 0.5) * overshootDistance target velo * 10)
+      , veloY + round (((rands H.! 1) - 0.5) * overshootDistance target velo * 10)
       )
+
+-- | Just exhaustively try all options to find starting velocity that results in
+-- highest reached Y-coordinate.
+bruteforce :: Target -> ((Int, Int), Int)
+bruteforce target@Target{..}
+  = maximumBy (comparing snd)
+  $ mapMaybe (\p -> (p,) <$> hitsTarget target p)
+    [(x, y) | x <- [1 .. right], y <- [top .. abs bottom * 10]]
 
 -- | The penalty for the solver. This is negative the maximum Y-coordinate
 -- unless the trajectory would not hit the target area, in which case it is the
@@ -54,7 +67,7 @@ solve target =
 solverPenalty :: Target -> (Int, Int) -> Double
 solverPenalty target initialVelo
   | Just maxYPos <- hitsTarget target initialVelo = fromIntegral (negate maxYPos)
-  | otherwise                                     = overshootDistance target initialVelo + 100
+  | otherwise                                     = (overshootDistance target initialVelo + 100) ** 2
 
 -- | Whether the given velocity causes the target to be hit. Returns the highest
 -- Y-position if it does.
@@ -98,8 +111,8 @@ parse = fromRight' . Parsec.parse pTarget ""
     fromRight' _         = error "This wasn't in our agreement!"
 
     pTarget :: Parsec String () Target
-    pTarget = Target <$> (string "target area: x=" *> pInt)      <*> (string ".." *> pInt)
-                     <*> (string ", y=-" *> pInt <* string "..") <*> pInt
+    pTarget = Target <$> (string "target area: x=" *> pInt)     <*> (string ".." *> pInt)
+                     <*> (string ", y=" *> pInt <* string "..") <*> pInt
 
     pInt :: Parsec String () Int
     pInt = read <$> many (digit <|> char '-')
